a9118bbaf78700305b3b48483377f2990e161635
Christopher Schwan <handgranaten-herbert@posteo.de>
2017-01-02 17:56:02 +0100

    Fix another compiler warning
    - the array random is not used when `switch = 2`

--- src/generator.f
+++ src/generator.f
@@ -250,7 +250,7 @@ c inv
         smin=mmin**2
         smax=(dsqrt(s(3))-mmax)**2
         call inv(
-     *    random,                                        ! random number
+     *    random(1),                                     ! random number
      *    s(ininv(ns,channel,generator)),                ! invariant mass
      *    ginv(i1),                                      ! local density
      *    mass(idhepinv(ns,channel,generator)),          ! mass
