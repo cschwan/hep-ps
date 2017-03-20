53cd0513e7a67b813e3ee9399fd73ab6ae0f8c18
Christopher Schwan <handgranaten-herbert@posteo.de>
2017-03-20 11:30:23 +0100

    Add missing copyright comments

--- include/hep/ps/fortran_helper.hpp
+++ include/hep/ps/fortran_helper.hpp
@@ -1,6 +1,24 @@
 #ifndef HEP_PS_FORTRAN_HELPER_HPP
 #define HEP_PS_FORTRAN_HELPER_HPP
 
+/*
+ * hep-ps - A C++ Library of Phase Space Integrands for High Energy Physics
+ * Copyright (C) 2017  Christopher Schwan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <vector>
 
 namespace hep
