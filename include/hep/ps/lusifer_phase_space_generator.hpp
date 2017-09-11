b9a875d388027b2fd7c9c49c02c242b8c19e4af9
Christopher Schwan <handgranaten-herbert@posteo.de>
2017-09-11 10:50:37 +0200

    Add some cosmetic fixes

--- include/hep/ps/lusifer_phase_space_generator.hpp
+++ include/hep/ps/lusifer_phase_space_generator.hpp
@@ -19,7 +19,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "hep/ps/luminosity_info.hpp"
 #include "hep/ps/lusifer_constants.hpp"
 #include "hep/ps/phase_space_generator.hpp"
 
