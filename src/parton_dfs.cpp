4cf60fcca699386896adae7d9b81f88af512a9bb
Christopher Schwan <handgranaten-herbert@posteo.de>
2018-02-06 15:49:29 +0100

    Implement the `register_partons` function

--- src/parton_dfs.cpp
+++ src/parton_dfs.cpp
@@ -4,6 +4,7 @@
 
 #include <algorithm>
 #include <iterator>
+#include <stdexcept>
 #include <utility>
 
 namespace hep
@@ -17,24 +18,17 @@ public:
 	impl(std::string const& pdf_name);
 
 	std::vector<std::unique_ptr<LHAPDF::PDF>> pdfs;
-	std::vector<double> xfx;
+	std::vector<std::pair<int, parton>> pdg_ids;
 };
 
 template <typename T>
 parton_dfs<T>::impl::impl(std::string const& pdf_name, std::size_t pdf_member)
-	: pdfs()
-	// TODO: is it possible to extract this number from LHAPDF?
-	, xfx(13)
 {
 	pdfs.emplace_back(LHAPDF::mkPDF(pdf_name, pdf_member));
 }
 
-// TODO: fix the implementation
 template <typename T>
 parton_dfs<T>::impl::impl(std::string const& pdf_name)
-	: pdfs()
-	// TODO: is it possible to extract this number from LHAPDF?
-	, xfx(13)
 {
 	for (auto pdf : LHAPDF::mkPDFs(pdf_name))
 	{
@@ -93,19 +87,10 @@ void parton_dfs<T>::eval(
 	std::vector<parton_array<T>>& scale_pdfs,
 	std::vector<parton_array<T>>& uncertainty_pdfs
 ) {
-	// TODO: is it possible to get these values from LHAPDF?
-	constexpr std::size_t cx = -4 + 6;
-	constexpr std::size_t sx = -3 + 6;
-	constexpr std::size_t ux = -2 + 6;
-	constexpr std::size_t dx = -1 + 6;
-	constexpr std::size_t gl =  0 + 6;
-	constexpr std::size_t dq =  1 + 6;
-	constexpr std::size_t uq =  2 + 6;
-	constexpr std::size_t sq =  3 + 6;
-	constexpr std::size_t cq =  4 + 6;
-
 	scale_pdfs.clear();
 	scale_pdfs.resize(scales.size());
+
+	auto const xval = static_cast <double> (x);
 
 	for (std::size_t i = 0; i != scales.size(); ++i)
 	{
@@ -118,21 +103,19 @@ void parton_dfs<T>::eval(
 
 		if (result == end)
 		{
-			pimpl->pdfs.front()->xfxQ(
-				static_cast <double> (x),
-				static_cast <double> (scales.at(i).factorization()),
-				pimpl->xfx
-			);
+			auto const muf =
+				static_cast <double> (scales.at(i).factorization());
 
-			scale_pdfs.at(i)[parton::anti_charm]   = T(pimpl->xfx[cx]) / x;
-			scale_pdfs.at(i)[parton::anti_strange] = T(pimpl->xfx[sx]) / x;
-			scale_pdfs.at(i)[parton::anti_up]      = T(pimpl->xfx[ux]) / x;
-			scale_pdfs.at(i)[parton::anti_down]    = T(pimpl->xfx[dx]) / x;
-			scale_pdfs.at(i)[parton::gluon]        = T(pimpl->xfx[gl]) / x;
-			scale_pdfs.at(i)[parton::down]         = T(pimpl->xfx[dq]) / x;
-			scale_pdfs.at(i)[parton::up]           = T(pimpl->xfx[uq]) / x;
-			scale_pdfs.at(i)[parton::strange]      = T(pimpl->xfx[sq]) / x;
-			scale_pdfs.at(i)[parton::charm]        = T(pimpl->xfx[cq]) / x;
+			for (auto const& ids : pimpl->pdg_ids)
+			{
+				auto const xfx = pimpl->pdfs.front()->xfxQ(
+					ids.first,
+					xval,
+					muf
+				);
+
+				scale_pdfs.at(i)[ids.second] = T(xfx) / x;
+			}
 		}
 		else
 		{
@@ -152,29 +135,34 @@ void parton_dfs<T>::eval(
 		uncertainty_pdfs.front() = scale_pdfs.front();
 	}
 
+	auto const muf = static_cast <double> (scales.front().factorization());
+
 	for (std::size_t i = 1; i != size; ++i)
 	{
-		pimpl->pdfs.front()->xfxQ(
-			static_cast <double> (x),
-			static_cast <double> (scales.front().factorization()),
-			pimpl->xfx
-		);
-
-		uncertainty_pdfs.at(i)[parton::anti_charm]   = T(pimpl->xfx[cx]) / x;
-		uncertainty_pdfs.at(i)[parton::anti_strange] = T(pimpl->xfx[sx]) / x;
-		uncertainty_pdfs.at(i)[parton::anti_up]      = T(pimpl->xfx[ux]) / x;
-		uncertainty_pdfs.at(i)[parton::anti_down]    = T(pimpl->xfx[dx]) / x;
-		uncertainty_pdfs.at(i)[parton::gluon]        = T(pimpl->xfx[gl]) / x;
-		uncertainty_pdfs.at(i)[parton::down]         = T(pimpl->xfx[dq]) / x;
-		uncertainty_pdfs.at(i)[parton::up]           = T(pimpl->xfx[uq]) / x;
-		uncertainty_pdfs.at(i)[parton::strange]      = T(pimpl->xfx[sq]) / x;
-		uncertainty_pdfs.at(i)[parton::charm]        = T(pimpl->xfx[cq]) / x;
+		for (auto const& ids : pimpl->pdg_ids)
+		{
+			auto const xfx = pimpl->pdfs.front()->xfxQ(ids.first, xval, muf);
+			uncertainty_pdfs.at(i)[ids.second] = T(xfx) / x;
+		}
 	}
 }
 
 template <typename T>
 void parton_dfs<T>::register_partons(parton_set set)
 {
+	for (auto const p : set)
+	{
+		int const pdg_id = parton_to_pdg_id(p);
+
+		if (!pimpl->pdfs.front()->hasFlavor(pdg_id))
+		{
+			throw std::domain_error("PDF doesn't support the requested flavor");
+		}
+
+		pimpl->pdg_ids.emplace_back(pdg_id, p);
+	}
+
+	pimpl->pdg_ids.shrink_to_fit();
 }
 
 // -------------------- EXPLICIT TEMPLATE INSTANTIATIONS --------------------
