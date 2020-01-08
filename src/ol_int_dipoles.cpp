cschwan/hep-ps

commit 3abf2ee8c617afb028ed9e566bd17a1751845adc

Calculate photon dipoles with the correct alpha
- OpenLoops sometimes uses a different alpha if there are external
  photons. By using the PDG id `-22` we make sure that the dipoles have
  the correct coupling constant

src/ol_int_dipoles.cpp
@@ -91,6 +91,15 @@ ol_int_dipoles<T>::ol_int_dipoles(
                     {
                         continue;
                     }
+
+                    // OpenLoops quirk: a -> ff~ without alpha(0)/alpha by using `-22` for the
+                    // photon
+                    auto const photon_index = dip.emitter()
+                        - ((dip.unresolved() < dip.emitter()) ? 1 : 0);
+
+                    assert( dipole_ids.at(photon_index) == pdg_id_of_photon() );
+
+                    dipole_ids.at(photon_index) = -pdg_id_of_photon();
                 }
             }
 

src/ol_real_matrix_elements.cpp
@@ -107,6 +107,15 @@ ol_real_matrix_elements<T>::ol_real_matrix_elements(
                     {
                         continue;
                     }
+
+                    // OpenLoops quirk: a -> ff~ without alpha(0)/alpha by using `-22` for the
+                    // photon
+                    auto const photon_index = dip.emitter()
+                        - ((dip.unresolved() < dip.emitter()) ? 1 : 0);
+
+                    assert( dipole_ids.at(photon_index) == pdg_id_of_photon() );
+
+                    dipole_ids.at(photon_index) = -pdg_id_of_photon();
                 }
             }
 

src/pdg_functions.cpp
@@ -44,7 +44,8 @@ final_state pdg_id_to_final_state(int id)
     case  16:
         return final_state::neutrino;
 
-    case 22:
+    case  22:
+    case -22: // OpenLoops quirk
         return final_state::photon;
 
     default:
@@ -69,6 +70,7 @@ parton pdg_id_to_parton(int id)
     case   5: return parton::bottom;
     case  21: return parton::gluon;
     case  22: return parton::photon;
+    case -22: return parton::photon; // OpenLoops quirk
 
     default:
         // TODO: NYI
@@ -107,6 +109,7 @@ particle_type pdg_id_to_particle_type(int id)
         return particle_type::fermion;
 
     case  22:
+    case -22: // OpenLoops quirk
     case  21:
         return particle_type::boson;
 
@@ -173,6 +176,7 @@ bool pdg_id_has_color(int id)
     case  14:
     case  16:
     case  22:
+    case -22: // OpenLoops quirk
         return false;
 
     default:
@@ -214,6 +218,7 @@ bool pdg_id_has_charge(int id)
     case  16:
     case  21:
     case  22:
+    case -22: // OpenLoops quirk
         return false;
 
     default:
@@ -418,6 +423,7 @@ int pdg_id_to_charge_times_three(int id)
     case  21:
     // photon
     case  22:
+    case -22: // OpenLoops quirk
         return 0;
 
     default:

commit b74f044d218bb213a8454ff0d89825c0a18d37a7

Fix bug introduced with commit 3abf2ee8
- fixed wrong assertion statement

src/ol_int_dipoles.cpp
@@ -92,14 +92,15 @@ ol_int_dipoles<T>::ol_int_dipoles(
                         continue;
                     }
 
-                    // OpenLoops quirk: a -> ff~ without alpha(0)/alpha by using `-22` for the
-                    // photon
                     auto const photon_index = dip.emitter()
                         - ((dip.unresolved() < dip.emitter()) ? 1 : 0);
 
-                    assert( dipole_ids.at(photon_index) == pdg_id_of_photon() );
-
-                    dipole_ids.at(photon_index) = -pdg_id_of_photon();
+                    // OpenLoops quirk: a -> ff~ without alpha(0)/alpha by using `-22` for the
+                    // photon
+                    if (dipole_ids.at(photon_index) == pdg_id_of_photon())
+                    {
+                        dipole_ids.at(photon_index) = -pdg_id_of_photon();
+                    }
                 }
             }
 

src/ol_real_matrix_elements.cpp
@@ -108,14 +108,15 @@ ol_real_matrix_elements<T>::ol_real_matrix_elements(
                         continue;
                     }
 
-                    // OpenLoops quirk: a -> ff~ without alpha(0)/alpha by using `-22` for the
-                    // photon
                     auto const photon_index = dip.emitter()
                         - ((dip.unresolved() < dip.emitter()) ? 1 : 0);
 
-                    assert( dipole_ids.at(photon_index) == pdg_id_of_photon() );
-
-                    dipole_ids.at(photon_index) = -pdg_id_of_photon();
+                    // OpenLoops quirk: a -> ff~ without alpha(0)/alpha by using `-22` for the
+                    // photon
+                    if (dipole_ids.at(photon_index) == pdg_id_of_photon())
+                    {
+                        dipole_ids.at(photon_index) = -pdg_id_of_photon();
+                    }
                 }
             }
 